91ac9e1ad10f7121dd0887c197ca9cce83282cd2
Aldihamda <aldihamda94@gmail.com>
2020-05-10 20:57:05 +0700

    yoloo

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 moveVector;
+
+    private float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move (moveVector * Time.deltaTime);
+    }
+}

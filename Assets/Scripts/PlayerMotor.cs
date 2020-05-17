50cd013791a8c758e2014f5b1a29546264c22d57
Aldihamda <aldihamda94@gmail.com>
2020-05-17 22:40:54 +0700

    haloo
    17/05/2020

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -8,6 +8,12 @@ public class PlayerMotor : MonoBehaviour
     private Vector3 moveVector;
 
     private float speed = 5.0f;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
+
+    private float animationDuration = 3.0f;
+
+    private bool isDead = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +24,52 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        controller.Move (moveVector * Time.deltaTime);
+
+        if (isDead)
+            return;
+
+        if (Time.time < animationDuration)
+        {
+            controller.Move (Vector3.forward * speed * Time.deltaTime);
+            return; 
+        }
+
+        moveVector = Vector3.zero;
+
+        if (controller.isGrounded) 
+        {
+            verticalVelocity = -0.5f;
+        }
+        else 
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        //X - Left and Right
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        //Y - Up and Down
+        moveVector.y = verticalVelocity;
+        //Z - Forward and Backward
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+    }
+
+    public void SetSpeed(float modifier)
+    {
+        speed = 5.0f + modifier;
+    }
+
+    //It is called every time u hits something
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.point.z > transform.position.z + controller.radius)
+            Death ();
+    }
+
+    private void Death()
+    {
+        isDead = true;
+        GetComponent<Score> ().onDeath ();
     }
 }
